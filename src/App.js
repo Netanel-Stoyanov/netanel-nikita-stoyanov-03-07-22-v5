5e95aac3158ae1d64df8b5978ec85ccb8d5c2b6e
netan <netanel.stoyanov@gmail.com>
2022-07-06 05:25:01 +0200

    final-project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import {Route, Switch} from "react-router-dom";
+import HomeSearchResult from "./page/home-search-result/HomeSearchResult";
+import FavoritePage from "./page/favorite-page/FavoritePage";
+import HomePage from "./page/home-page/HomePage";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    return (
+        <div>
+            <Switch>
+                <Route path="/" exact component={HomePage}/>
+                <Route path="/:key/:city" exact component={HomeSearchResult}/>
+                <Route path="/favorite" exact component={FavoritePage}/>
+            </Switch>
+        </div>
+    );
 }
 
 export default App;
